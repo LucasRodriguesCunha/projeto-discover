5b4908d3137181892054f5ea56f7c84523a3816a
Lucas Rodrigues Cunha <123827882+LucasRodriguesCunha@users.noreply.github.com>
2023-07-05 23:45:16 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+function alterarModo() {
+    const html = document.documentElement;
+    html.classList.toggle("light");
+
+    // Pegar a tag img
+    const img = document.querySelector("#perfil img");
+
+    // Substituir a imagem
+
+    if (html.classList.contains("light")) {
+        // Se tiver light mode, adicionar a imagem light
+        img.setAttribute("src", "../assets/assets/avatar-light.png");
+    } else {
+        // Se tiver sem light mode, manter a imagem normal 
+        img.setAttribute("src", "../assets/assets/avatar.png");
+
+    }
+
+
+
+
+
+
+}
